845afc1ee35e3cff7f77127bd403c257b6d423b3
Dipti1107 <85673091+Dipti1107@users.noreply.github.com>
2022-02-06 19:39:19 +0530

    Update ev_odd.java

--- Web-Tech/java-input-basic/ev_odd.java
+++ Web-Tech/java-input-basic/ev_odd.java
@@ -4,7 +4,7 @@ public class ev_odd {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n;
-        System.out.print("Enter a No. : ");
+        System.out.print("Enter a Number : ");
         n = sc.nextInt();
         if (n % 2 == 0)
             System.out.println("It is an Even number");
